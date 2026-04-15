42501618217a4bc3900186d5824d36810429e4ea
njomzagashii <njomza.gashi10@student.uni-pr.edu>
2026-04-16 00:48:28 +0200

    Permiresova kodin

--- server.php
+++ server.php
@@ -1,10 +1,11 @@
 <?php
 
-$host = "0.0.0.0";
+$host = "0.0.0.0"; 
 $tcp_port = 9000;
-$http_port = 8080;
+$http_port = 9090; 
 $max_clients = 6;
-$timeout_seconds = 300;
+$timeout_seconds = 300; 
+
 
 $clients = [];
 $messages_log = [];
@@ -16,6 +17,7 @@ socket_bind($main_socket, $host, $tcp_port);
 socket_listen($main_socket, $max_clients);
 socket_set_nonblock($main_socket);
 
+
 $http_socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($http_socket, SOL_SOCKET, SO_REUSEADDR, 1);
 socket_bind($http_socket, $host, $http_port);
@@ -25,26 +27,26 @@ socket_set_nonblock($http_socket);
 echo "TCP Server nisi ne portin $tcp_port...\n";
 echo "HTTP Monitoring nisi ne portin $http_port...\n";
 
-while(true) {
-
-    $read = [$main_socket, $http_socket];
-
+while (true) {
+    $read = [];
+    $read[] = $main_socket;
+    $read[] = $http_socket;
+    
     foreach ($clients as $c) {
         $read[] = $c['socket'];
     }
 
     $write = null;
     $except = null;
-
+    
     if (socket_select($read, $write, $except, 1) < 1) {
-        
+        checkTimeouts();
         continue;
     }
 
     
     if (in_array($main_socket, $read)) {
         $new_socket = @socket_accept($main_socket);
-
         if ($new_socket) {
             if (count($clients) >= $max_clients) {
                 $msg = "Serveri plot. Provo me vone.\n";
@@ -53,117 +55,46 @@ while(true) {
             } else {
                 socket_getpeername($new_socket, $ip);
                 $socket_id = (int)$new_socket;
-
                 $clients[$socket_id] = [
                     'socket' => $new_socket,
                     'ip' => $ip,
                     'last_seen' => time(),
                     'requests' => 0
                 ];
-
                 if ($admin_client === null) {
                     $admin_client = $socket_id;
-                    echo "Admin i ri: $ip\n";
+                    echo "Admin i ri lidhur: $ip\n";
                 } else {
-                    echo "Klient i ri: $ip\n";
+                    echo "Klient i ri lidhur: $ip\n";
                 }
             }
         }
     }
 
-   
     if (in_array($http_socket, $read)) {
-        handleHttpRequest($http_socket, $clients, $messages_log);
+        handleHttpRequest($http_socket);
     }
 
-    
+   
     foreach ($clients as $id => $client) {
-
         if (in_array($client['socket'], $read)) {
+            
+           
+            $is_admin = ($id === $admin_client);
+            if (!$is_admin) {
+                usleep(300000); 
+            }
 
-            $input = @socket_read($client['socket'], 1024);
-
+            $input = @socket_read($client['socket'], 1024 * 1024);
+            
             if ($input === false || $input === "") {
                 closeConnection($id);
                 continue;
             }
 
             $input = trim($input);
-
             $clients[$id]['last_seen'] = time();
-            $clients[$id]['requests']++;
+            $clients[$id]['requests']++; 
+            $messages_log[] = ["ip" => $client['ip'], "msg" => substr($input, 0, 50), "time" => date("H:i:s")];
 
-            $messages_log[] = [
-                "ip" => $client['ip'],
-                "msg" => substr($input, 0, 50),
-                "time" => date("H:i:s")
-            ];
-
-            $is_admin = ($id === $admin_client);
             $response = "";
-
-        
-            if (strpos($input, '/') === 0) {
-
-                $parts = explode(' ', $input, 3);
-                $command = $parts[0];
-                $arg1 = $parts[1] ?? '';
-                $arg2 = $parts[2] ?? '';
-
-                $admin_only_commands = ['/delete','/upload','/execute'];
-
-                if (!$is_admin && in_array($command, $admin_only_commands)) {
-                    $response = "Error: Vetem admin!";
-                } else {
-
-                    switch($command) {
-
-                        case '/list':
-                            $files = array_diff(scandir("."), ['.','..']);
-                            $response = "Files: " . implode(", ", $files);
-                            break;
-
-                        case '/read':
-                            if (file_exists($arg1) && !is_dir($arg1)) {
-                                $response = file_get_contents($arg1);
-                            } else {
-                                $response = "Error: File nuk ekziston.";
-                            }
-                            break;
-
-                        case '/info':
-                            if (file_exists($arg1)) {
-                                $response = "INFO: $arg1 | "
-                                    . filesize($arg1) . " bytes | "
-                                    . date("Y-m-d H:i", filemtime($arg1));
-                            } else {
-                                $response = "Error: Nuk u gjet.";
-                            }
-                            break;
-
-                        case '/search':
-                            $files = scandir(".");
-                            $found = array_filter($files, fn($f) => strpos($f, $arg1) !== false);
-                            $response = "Rezultatet: " . implode(", ", $found);
-                            break;
-
-                        case '/delete':
-                            if (file_exists($arg1)) {
-                                unlink($arg1);
-                                $response = "File u fshi.";
-                            } else {
-                                $response = "Error: Nuk u gjet.";
-                            }
-                            break;
-
-                        default:
-                            $response = "Komande e panjohur.";
-                    }
-                }
-            }
-
-            socket_write($client['socket'], $response . "\n");
-        }
-    }
-
-} 
